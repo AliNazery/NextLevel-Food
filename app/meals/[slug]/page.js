ea4798f3144c865daf959e75245783e763c8192d
Ali Nazery <ali.nazery@example.com>
2024-06-11 11:14:17 +0430

    second commit

--- app/meals/[slug]/page.js
+++ app/meals/[slug]/page.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import Image from 'next/image';
+import classes from './page.module.css'
 
-export default function Detail() {
+export default function MealDetailsPage() {
   return (
-    <div>
-      <h2>the melas detials</h2>
-    </div>
-  )
+    <>
+      <header className={classes.header}>
+        <div className={classes.image}>
+          <Image fill/>
+        </div>
+        <div className={classes.headerText}>
+          <h1></h1>
+        </div>
+      </header>
+      <main></main>
+    </>
+  );
 }
--- app/meals/page.js
+++ app/meals/page.js
@@ -1,11 +1,36 @@
+import React, { Suspense } from 'react'
+import classes from './page.module.css'
 import Link from 'next/link'
-import React from 'react'
+import MealsGrid from '@/components/meals/meals-grid'
+import { getMeals } from '@/lib/meals'
 
-export default function Meal() {
+async function Meals() {
+  const meals = await getMeals();
+  return <MealsGrid meals={meals} />;
+}
+
+export default function MealsPage() {
   return (
-    <div>
-      <h2>The meal</h2>
-      <Link href={'/community'}>THe community</Link>
-    </div>
-  )
+    <>
+      <header className={classes.header}>
+        <h1>
+          Delicious meals, created{" "}
+          <span className={classes.highlight}>by you</span>
+        </h1>
+        <p>
+          Choose your favorite recipe and cook it yourself. it is easy and fun
+        </p>
+        <p className={classes.cta}>
+          <Link href="/meals/share">Share your Favorite Recipe</Link>
+        </p>
+      </header>
+      <main className={classes.main}>
+        <Suspense
+          fallback={<p className={classes.loading}>Fetching Meals ...</p>}
+        >
+          <Meals />
+        </Suspense>
+      </main>
+    </>
+  );
 }
